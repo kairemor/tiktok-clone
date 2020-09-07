523640f43043ec0a226d3b296df2adad63458223
kmtc44 <kairmor@yahoo.com>
2020-09-07 22:52:45 +0000

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMh678yYIU2sOODl6AXXVJr_27jvDLnpk",
-  authDomain: "tik-tok-clone-eb635.firebaseapp.com",
-  databaseURL: "https://tik-tok-clone-eb635.firebaseio.com",
-  projectId: "tik-tok-clone-eb635",
-  storageBucket: "tik-tok-clone-eb635.appspot.com",
-  messagingSenderId: "602358408631",
-  appId: "1:602358408631:web:a32f1830ccc2d942f2f432",
-  measurementId: "G-38YHL3X30F",
+  apiKey: "AIzaSyCzlAmGNQwPN_W2hZEhEzemn8NQ5bJ67HY",
+  authDomain: "tiktok-clone-ee47c.firebaseapp.com",
+  databaseURL: "https://tiktok-clone-ee47c.firebaseio.com",
+  projectId: "tiktok-clone-ee47c",
+  storageBucket: "tiktok-clone-ee47c.appspot.com",
+  messagingSenderId: "389525572897",
+  appId: "1:389525572897:web:2ea7f23a9da8d64176c260",
+  measurementId: "G-BMRK416M01"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
